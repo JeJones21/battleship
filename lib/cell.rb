a2319485346eec4c7276e79181f389b02843e888
Jessica Jones <daddehjones@gmail.com>
2021-07-13 20:57:58 -0400

    Create method empty?.

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,10 +1,19 @@
 class Cell
 
   attr_reader :coordinate,
-              :ship 
+              :ship
 
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
   end
+
+  def empty?
+    if @ship == nil
+      return true
+    else
+      return false
+    end
+  end
+
 end
