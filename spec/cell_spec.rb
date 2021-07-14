d38ebf6c48fa173dbe49cb524a841ca4ab6f8e04
Jessica Jones <daddehjones@gmail.com>
2021-07-13 21:12:15 -0400

    Create fired_upon? test.

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -31,4 +31,14 @@ RSpec.describe Cell do
     expect(cell.empty?).to eq(false)
   end
 
+  it "can tell when it is fired upon" do
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell.place_ship(cruiser)
+
+    expect(cell.fired_upon?).to eq(false)
+
+  end
+
 end
