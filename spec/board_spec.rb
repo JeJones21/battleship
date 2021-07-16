b55500d32d5b42a9d0a4aa1eb6c1bb8431beecaa
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-07-15 20:09:53 -0600

    Create validate placement test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -28,4 +28,15 @@ RSpec.describe Board do
     expect(board.valid_coordinate?("E1")).to eq(false)
     expect(board.valid_coordinate?("A22")).to eq(false)
   end
+
+  it "can validate placement" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    board.cell_setup
+
+    expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+  end
 end
