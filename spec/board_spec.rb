77b89444e2e7629e9038ca113d29c4c41c0d60e3
Jessica Jones <daddehjones@gmail.com>
2021-07-17 15:17:12 -0400

    Create test to compare helper methods.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -40,34 +40,34 @@ RSpec.describe Board do
     expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
   end
 
-#   it "can validate placement by consecutively" do
-#     board = Board.new
-#     cruiser = Ship.new("Cruiser", 3)
-#     submarine = Ship.new("Submarine", 2)
-#
-#     board.cell_setup
-#
-#     expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
-#     expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
-#     expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
-#     expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
-#   end
-#
-#   it "can compare all of these bs methods" do
-#     board = Board.new
-#     cruiser = Ship.new("Cruiser", 3)
-#     submarine = Ship.new("Submarine", 2)
-#
-#     board.cell_setup
-#
-#     expect(board.valid_placement?(cruiser, ["A1","A2", "A3"])).to eq(true)
-#
-#   end
-#
-#
-#
-#
-#
-#
-#
-# end
+  it "can validate placement by consecutively" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    board.cell_setup
+
+    expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
+    expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
+  end
+
+  xit "can compare all of these bs methods" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    board.cell_setup
+
+    expect(board.valid_placement?(cruiser, ["A1","A2", "A3"])).to eq(true)
+  end
+
+
+
+
+
+
+
+
+end
