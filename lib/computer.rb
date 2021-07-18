d7803026b469e19540aaeb3ed6147a19a135152d
Jessica Jones <daddehjones@gmail.com>
2021-07-18 17:11:39 -0400

    Initialize computer board.

---
+++ lib/computer.rb
@@ -0,0 +1,15 @@
+class Computer
+
+  attr_reader :comp_board
+
+  def initialize(comp_board)
+    @comp_board = comp_board
+    @cruise_ship = Ship.new("Cruiser", 3)
+    @sub =
+  end
+
+  def add_ships(ship)
+
+
+  end
+end
