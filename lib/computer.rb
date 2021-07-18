dce65d9f5796a5b158f92b3059f396bf23e5f937
Jessica Jones <daddehjones@gmail.com>
2021-07-18 17:25:40 -0400

    Create add_ships method. Add ships to initialize.

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,15 +1,22 @@
 class Computer
 
-  attr_reader :comp_board
+  attr_reader :comp_board,
+              :ships,
+              :cruise_ship,
+              :sub
 
   def initialize(comp_board)
     @comp_board = comp_board
     @cruise_ship = Ship.new("Cruiser", 3)
-    @sub =
+    @sub = Ship.new("Submarine", 2)
+    @ships = []
   end
 
   def add_ships(ship)
+    @ships << ship
+  end
 
+  def random_placement
 
   end
 end
