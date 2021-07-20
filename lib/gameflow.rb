726b37eb16decc32cb51a1093ede6ec9f46ee604
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-07-19 23:34:32 -0600

    Create ready_to_play, place_coordinates, display_current_computer_board, display_current_player_board, fire_missle, take_turn methods

--- lib/gameflow.rb
+++ lib/gameflow.rb
@@ -21,6 +21,10 @@ class Gameflow
   def start
     welcome_message
     @board.render
+    ready_to_play
+    display_current_computer_board
+    display_current_player_board
+    fire_missle
   end
 
   def welcome_message
@@ -53,38 +57,37 @@ class Gameflow
     puts "Ships can be horizontal or vertical."
     puts "However, they cannot be overlapping or diagonal."
     puts "Coordinates need to be consecutive without skipping (Ex: 'A1 A2 A3')"
+    puts "Include one space between each coordinate."
     puts "****************************************"
-    puts "Now enter 2 coordinates for your cruiser"
+    puts "\nNow enter 3 coordinates for your cruiser"
     puts @board.render(true)
+
     @player_input = input.upcase.split
+    board.valid_placement?(cruiser, @player_input)
 
-    unless board.valid_placement?(cruiser, @player_input)
-      invalid
-    end
-
-    until board.valid_placement?(cruiser, @player_input)
-      instructions
-      unless board.valid_placement?(cruiser, @player_input)
-        invalid
-      end
+    while !board.valid_placement?(cruiser, @player_input)
+      puts "The coordinates #{@player_input} are not valid.\n"
+      puts "Enter 3 coordinates for your cruiser."
+      @player_input = input.upcase.split
     end
 
     board.place(cruiser, @player_input)
+    puts "\n"
+    puts @board.render(true)
 
-    puts "Now enter 3 coordinates for your submarine"
+    puts "\nNow enter 2 coordinates for your submarine"
+    @player_input = input.upcase.split
+    board.valid_placement?(submarine, @player_input)
 
-    unless board.valid_placement?(submarine, @player_input)
-      invalid
-    end
-
-    until board.valid_placement?(submarine, @player_input)
-      instructions
-      unless board.valid_placement?(submarine, @player_input)
-        invalid
-      end
+    while !board.valid_placement?(submarine, @player_input)
+      puts "The coordinates #{@player_input} are not valid.\n"
+      puts "Enter 2 coordinates for your submarine."
+      @player_input = input.upcase.split
     end
 
     board.place(submarine, @player_input)
+    puts "\n"
+    puts @board.render(true)
   end
 
   def ship_placement
@@ -96,8 +99,7 @@ class Gameflow
   end
 
   def instructions
-    puts "\n"
-    puts "You and the computer both receive a board and two ships."
+    puts "\nYou and the computer both receive a board and two ships."
     puts "After you place your ships on the board..."
     puts "You'll both take turns firing shots."
     puts "A hit ship will reveal an 'H'"
@@ -118,16 +120,60 @@ class Gameflow
   end
 
   def play
-    puts "Great decision! My ships are in play!"
-    puts "Are you to battle?"
+    puts "\nGreat decision! My ships are in play!"
+    puts "Are you ready to battle?"
     puts "Place your ships now."
-    puts "Your cruiser is 3 hits long and submarine is 2 hits long."
-    puts "Your board is a 4x4. (Rows: A - D, Columns: 1 - 4)"
-    puts "\n"
+    puts "\nYour cruiser is 3 hits long and submarine is 2 hits long."
+    puts "Your board is a 4x4. (Rows: A - D, Columns: 1 - 4)\n"
     place_coordinates
   end
 
   def quit
     exit
+  end
+
+
+
+
+
+
+  # Once the computer and player ships are placed, let the player know play begins
+  def ready_to_play
+    puts "\nIt looks like you're all set!"
+    puts "Now get ready, because you get to fire first."
+  end
+
+  def take_turn
+    # 1. Displaying the boards
+      # 1a. Display computer board (reveal: false)
+      # 1b. Display player board (reveal: true)
+    # 2. Player choosing a coordinate to fire on
+      # 2a. Check if user coordinate entered is valid
+    # 3. Computer choosing a coordinate to fire on
+    # 4. Reporting the result of the Player’s shot
+    # 5. Reporting the result of the Computer’s shot
+
+    # All of these are currently being called in 'start' but will be called in take_turn
+    # display_current_computer_board
+    # display_current_player_board
+    # fire_missle
+
+  end
+
+  def display_current_computer_board
+    puts "\n=============COMPUTER BOARD=============\n"
+    puts @wizard.wiz_board.render
+  end
+
+  def display_current_player_board
+    puts "\n=============PLAYER BOARD=============\n"
+    puts @board.render(true)
+  end
+
+  def fire_missle
+    puts "\nEnter the coordinate for your shot:"
+    @player_input = input.upcase
+    puts "\nNext is the firing upon the computer board."
+    @wizard.wiz_board.fire_on_this_coordinate(@player_input)
   end
 end
