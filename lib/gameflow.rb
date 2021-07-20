150d050aa55d61ec1802223a52e776efc925b3d2
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-07-19 19:22:13 -0600

    Update instructions and play methods

--- lib/gameflow.rb
+++ lib/gameflow.rb
@@ -1,22 +1,25 @@
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/computer'
+
 class Gameflow
 
   attr_reader :board,
-              :player,
               :wizard,
               :cruiser,
               :submarine
 
   def initialize
     @board = Board.new
-    @player = Board.new
-    @wizard = Computer.new(wiz_board)
+    player = Board.new
+    @wizard = Computer.new(player)
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
   end
 
   def start
     welcome_message
-    instructions
     @board.render
   end
 
@@ -40,6 +43,10 @@ class Gameflow
 
   end
 
+  def place_coordinates
+    
+  end
+
   def ship_placement
 
   end
@@ -49,13 +56,35 @@ class Gameflow
   end
 
   def instructions
-    if condition
-
+    puts "\n"
+    puts "You and the computer both receive a board and two ships."
+    puts "After you place your ships on the board..."
+    puts "You'll both take turns firing shots."
+    puts "A hit ship will reveal an 'H'"
+    puts "A miss will reveal an 'M'"
+    puts "The first player to sink all of the opponent's ships wins!"
+    puts "Would you like to play or quit?"
+    puts "\n"
+    if answer == 'p'
+      play
+    elsif answer == 'q'
+      quit
+    elsif answer == 'i'
+      instructions
+    else
+      invalid
+      welcome_message
     end
   end
 
   def play
-    # say to user, then place coordinates
+    puts "Great decision! My ships are in play!"
+    puts "Are you to battle?"
+    puts "Place your ships now."
+    puts "Your cruiser is 3 hits long and submarine is 2 hits long."
+    puts "Your board is a 4x4. (Rows: A - D, Columns: 1 - 4)"
+    puts "\n"
+    place_coordinates
   end
 
   def quit
